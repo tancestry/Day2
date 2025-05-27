9c65af0279de6ee849ff50f630762bc155a001e3
Muya Xu <muyaxu8@gmail.com>
2025-05-27 16:49:05 -0700

    Changes have been made

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ int main()  {
     brain myBrain;
 
     // Set up left and right motors
-    motor leftMotor = motor(PORT1, 2.0, true);
-    motor rightMotor = motor(PORT6, 2.0, false);
+    motor leftMotor = motor(PORT7, 2.0, true);
+    motor rightMotor = motor(PORT12, 2.0, false);
 
     // Start those motors
     leftMotor.spin(forward, 50, percent);
@@ -27,22 +27,24 @@ int main()  {
     // Stop
     leftMotor.stop();
     rightMotor.stop();
-
+    //Something is fishy
     // Now go backwards
     leftMotor.spin(forward, -50, percent);
     rightMotor.spin(forward, -50, percent);
-    
+    myBrain.Screen.clearScreen();
     // Let it go a bit
     wait(2, sec);
-
+    // Stop
     // TODO: Then stop
-
+    leftMotor.stop();
+    rightMotor.stop();
     // Print something
     myBrain.Screen.printAt(2, 30, "All done");
-    myBrain.playSound(vex::soundType::doorClose);
-
+    myBrain.playSound(vex::soundType::tollBooth);
+    printf("\nHello\n");
     while(true) {
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
 }
+    
