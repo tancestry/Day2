5763b466d5908e653b8f97de6fafa638bf77b3fe
kira <kira.the.whip@gmail.com>
2025-05-29 12:44:36 -0700

    Added stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
-/*————————————————————————————————————————————————————————————————————————————*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       kira                                                      */
-/*    Created:      5/18/2025, 3:23:38 PM                                     */
-/*    Description:  IQ2 project                                               */
-/*                                                                            */
-/*————————————————————————————————————————————————————————————————————————————*/
-#include "vex.h"
+    /*————————————————————————————————————————————————————————————————————————————*/
+    /*                                                                            */
+    /*    Module:       main.cpp                                                  */
+    /*    Author:       kira                                                      */
+    /*    Created:      5/18/2025, 3:23:38 PM                                     */
+    /*    Description:  IQ2 project                                               */
+    /*                                                                            */
+    /*————————————————————————————————————————————————————————————————————————————*/
+    #include "vex.h"
 
-using namespace vex;
+    using namespace vex;
 
-void driveForward(bool comeHome = true) {
- 
+    void driveForward(bool comeHome = true) {
+
     // Here's a little change
     brain myBrain;
 
     // Set up left and right motors
-   motor leftMotor = motor(PORT1, 2.0, true);
-     motor rightMotor = motor(PORT6, 2.0, false);
-     motor strangeMotor = motor(2,2.0,true);
+    motor leftMotor = motor(PORT1, 2.0, true);
+    motor rightMotor = motor(PORT6, 2.0, false);
+    motor strangeMotor = motor(2,2.0,true);
 
     // Start those motors
     leftMotor.spin(forward, 50, percent);
@@ -31,53 +31,56 @@ void driveForward(bool comeHome = true) {
     leftMotor.stop();
     rightMotor.stop();
 
-   if (comeHome == true) {
-        // Now go backwards
-        leftMotor.spin(forward, -50, percent);
-        rightMotor.spin(forward, -50, percent);
+    if (comeHome == true) {
+    // Now go backwards
+    leftMotor.spin(forward, -50, percent);
+    rightMotor.spin(forward, -50, percent);
 
-        // Let it go a bit
-        wait(2, sec);
+    // Let it go a bit
+    wait(2, sec);
 
-        // TODO: Then stop
-        leftMotor.stop();
-        rightMotor.stop();
+    // TODO: Then stop
+    leftMotor.stop();
+    rightMotor.stop();
     }
     // Print something
     myBrain.Screen.printAt(2, 30, "Ratchetttttttt!!!");
     myBrain.playSound(vex::soundType::ratchet);
 
-}
+    }
 
-int main()  {
+    int main()  {
     // Using a counter in a while loop
     int i = 0;
     while(i++ < 3) {
-        printf("Loop #%d\n", i+1);
-        driveForward();
+    printf("Loop #%d\n", i+1);
+    driveForward();
     }
     driveForward(false);
     while(true) {
-        
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
+
+    // Allow other tasks to run
+    this_thread::sleep_for(10);
     }
-}
+    }
 
-//axolotlprogramming is sigma and crazy
-//everyone is awsome! Like @axolotlprogramming and @sophief123!!
+    //axolotlprogramming is sigma and crazy
+    //everyone is awsome! Like @axolotlprogramming and @sophief123!!
 
- 
 
-/*————————————————————————————————————————————————————————————————————————————*/
-/*          So, anywhay I am about 9 years old and:                           */
-/*                                                                            */
-/*           Name: Kira                                                       */
-/*                              Hello hello                                   */
-/*                                                                            */
-/*                                                                            */
-/*————————————————————————————————————————————————————————————————————————————*/
 
-double banana;
-int yellow = 36;
-int answer = 2+8;
+    /*————————————————————————————————————————————————————————————————————————————*/
+    /*          So, anywhay I am about 9 years old and:                           */
+    /*                                                                            */
+    /*           Name: Kira                                                       */
+    /*                              Hello hello                                   */
+    /* oh my goodny! Siiggmmaa skkiibbiiddii ya!                                  */
+    /*                                                                            */
+    /*                                                                            */
+    /*————————————————————————————————————————————————————————————————————————————*/
+
+    // I LIKE BANANAS! SOOOOOOO MUUUUUUUCCCCCCCCHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!
+    double banana;
+    int yellow = 36;
+    int answer = 2+8;
+    // I 🩷 C++
